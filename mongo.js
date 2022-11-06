9934e0f1a5ea0880b655437a98bac6e3fabb6f58
jacob <jacob@stellardev.org>
2022-11-06 00:46:51 -0700

    Update Mongo Crendentials

--- mongo.js
+++ mongo.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb')
 class Mongo {
 
     static async connect() {
-        const credentials = 'X509-cert-2111527907155565647.pem'
+        const credentials = 'X509-cert-1301818279006279178.pem'
         const client = new MongoClient('mongodb+srv://hopelandsystems.dobnt5r.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
             sslKey: credentials,
             sslCert: credentials,
