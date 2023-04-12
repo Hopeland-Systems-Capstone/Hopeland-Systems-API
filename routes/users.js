abc5be751769eaa26fbcb9de000c3e7fb05b3787
Ryan Park <45363204+rynprrk@users.noreply.github.com>
2023-04-11 21:07:48 -0700

    Add addCard API Route
    Added POST API Route for addCard
    POST /users/addCard?key=apikey&user_id=user_id&card_number=card_number&name_on_card=name_on_card&card_expiration=card_expiration&cvc=cvc&address1=address1&address2=address2&city=city&state=state&zip=zip

--- routes/users.js
+++ routes/users.js
@@ -19,8 +19,6 @@ const api_key_util = require('./util/api_key_util');
 //PUT | /users?key=apikey&username=name&alert_id=alert_id | Add alert to user when given alert_id and username 
 //PUT | /users?key=apikey&email=email&salert_id=alert_id | Add alert to user when given alert_id and email
 
-//POST | /users/addCard?key=apikey&user_id=user_id&card_number=card_number&name_on_card=name_on_card&card_expiration=card_expiration&cvc=cvc&address1=address1&address2=address2&city=city&state=state&zip=zip
-//DELETE | /users/deleteCard?key=apikey?user_id=user_id?card_id=card_id
 //PUT | /users/setActiveCard?key=apikey&user_id=user_id&card_id=card_id
 //PUT | /users/updateCard?key=apikey&user_id=user_id&card_id=card_id&card_number=card_number&name_on_card=name_on_card&card_expiration=card_expiration&cvc=cvc&address1=address1&address2=address2&city=city&state=state&zip=zip
 //PUT | /users/setTimeZone?key=apikey&user_id=user_id&timezone=timezone
@@ -29,11 +27,11 @@ const api_key_util = require('./util/api_key_util');
 //GET | /users/getTimeZone?key=apikey&user_id=user_id
 router.get("/getTimeZone", limiter, async (req, res, next) => {
 
-    if(!await api_key_util.checkKey(res, req.query.key)) return;
+    if (!await api_key_util.checkKey(res, req.query.key)) return;
 
     const user_id = req.query.user_id;
 
-    if(user_id) {
+    if (user_id) {
         const timezone = await users_api.getTimezone(user_id);
         return res.status(200).json(timezone);
     }
@@ -44,11 +42,11 @@ router.get("/getTimeZone", limiter, async (req, res, next) => {
 //GET | /users/getActiveCard?key=apikey&user_id=user_id
 router.get("/getActiveCard", limiter, async (req, res, next) => {
 
-    if(!await api_key_util.checkKey(res, req.query.key)) return;
+    if (!await api_key_util.checkKey(res, req.query.key)) return;
 
     const user_id = req.query.user_id;
 
-    if(user_id) {
+    if (user_id) {
         const activeCard = await users_api.getActiveCard(user_id);
         return res.status(200).json(activeCard);
     }
@@ -59,11 +57,11 @@ router.get("/getActiveCard", limiter, async (req, res, next) => {
 //GET | /users/getCards?key=apikey&user_id=user_id
 router.get("/getCards", limiter, async (req, res, next) => {
 
-    if(!await api_key_util.checkKey(res, req.query.key)) return;
+    if (!await api_key_util.checkKey(res, req.query.key)) return;
 
     const user_id = req.query.user_id;
 
-    if(user_id) {
+    if (user_id) {
         const cards = users_api.getCards(user_id);
         return res.status(200).json(cards);
     }
@@ -71,14 +69,49 @@ router.get("/getCards", limiter, async (req, res, next) => {
     return res.status(400).json({ error: 'Invalid arguments.' });
 });
 
+//POST | /users/addCard?key=apikey&user_id=user_id&card_number=card_number&name_on_card=name_on_card&card_expiration=card_expiration&cvc=cvc&address1=address1&address2=address2&city=city&state=state&zip=zip
+router.post("/addCard", limiter, async (req, res, next) => {
+
+    if (!await api_key_util.checkKey(res, req.query.key)) return;
+
+    const user_id = req.query.user_id;
+    const card_number = req.query.card_number;
+    const name_on_card = req.query.name_on_card;
+    const card_expiration = req.query.card_expiration;
+    const cvc = req.query.cvc;
+    const address1 = req.query.address1;
+    const address2 = req.query.address2;
+    const city = req.query.city;
+    const state = req.query.state;
+    const zip = req.query.zip;
+
+    if (!user_id || !card_number || !name_on_card || !card_expiration || !cvc || !address1 || !city || !state || !zip) {
+        return res.status(400).json({ error: 'Invalid arguments.' });
+    }
+
+    try {
+        if(await users_api.addCard(user_id, card_number, name_on_card, card_expiration, cvc, address1, address2, city, state, zip))  {
+            return res.status(201).json({ message: 'Card created successfully.' });
+        } else {
+            return res.status(500).json({ message: 'Error adding card to user.' });
+        }
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Error adding card to user.' });
+    }
+});
+
+//DELETE | /users/deleteCard?key=apikey?user_id=user_id?card_id=card_id
+
 router.get("/", limiter, async (req, res, next) => {
 
-    if (!await api_key_util.checkKey(res,req.query.key)) return;
+    if (!await api_key_util.checkKey(res, req.query.key)) return;
 
     const username = req.query.username;
     const email = req.query.email;
     const hashed_password = req.query.hashed_password;
-    
+
     if (hashed_password) {
         if (username) {
             const valid = await users_api.verifyUsernamePasswordCombo(username, hashed_password);
@@ -110,7 +143,7 @@ router.get("/", limiter, async (req, res, next) => {
 
 router.post("/", limiter, async (req, res, next) => {
 
-    if (!await api_key_util.checkKey(res,req.query.key)) return;
+    if (!await api_key_util.checkKey(res, req.query.key)) return;
 
     const username = req.query.username;
     const email = req.query.email;
@@ -137,7 +170,7 @@ router.post("/", limiter, async (req, res, next) => {
 
 router.delete("/", limiter, async (req, res, next) => {
 
-    if (!await api_key_util.checkKey(res,req.query.key)) return;
+    if (!await api_key_util.checkKey(res, req.query.key)) return;
 
     const user_id = req.query.user_id;
     const username = req.query.username;
@@ -156,7 +189,7 @@ router.delete("/", limiter, async (req, res, next) => {
             return res.status(400).json({ error: 'Invalid arguments.' });
         }
     }
-    
+
     if (alert_id) {
         if (username) {
             await users_api.removeAlertFromUserWithUsername(username, alert_id);
@@ -183,7 +216,7 @@ router.delete("/", limiter, async (req, res, next) => {
 
 router.put("/", limiter, async (req, res, next) => {
 
-    if (!await api_key_util.checkKey(res,req.query.key)) return;
+    if (!await api_key_util.checkKey(res, req.query.key)) return;
 
     const username = req.query.username;
     const email = req.query.email;
@@ -201,7 +234,7 @@ router.put("/", limiter, async (req, res, next) => {
             return res.status(400).json({ error: 'Invalid arguments.' });
         }
     }
-    
+
     if (alert_id) {
         if (username) {
             await users_api.addAlertToUserWithUsername(username, alert_id);
